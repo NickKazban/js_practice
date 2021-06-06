3ad95bbbbc42317fbed39b427cfafad99cc4beda
NickKazban <mykola.kazban@gmail.com>
2021-06-06 22:34:32 +0300

    ошибка в html
    проверить, что передается через путь к js

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,5 +1,34 @@
+function _createModal(options) {
+    const modal = document.createElement(tagName => 'div')
+    modal.insertAdjacentHTML(where => 'afterbegin', html => `
+        <div class="prac__modal">
+            <div class="modal-overlay">
+                <div class="modal-window">
+                    <div class="modal-header">
+                        <span class="modal-title">Modal title</span>
+                        <span class="modal-close">&times;</span>
+                    </div>
+                    <div class="modal-body">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, natus.</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, natus.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button>
+                            OK
+                        </button>
+                        <button>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+    return modal;
+}
+
 $.modal = function (options) {
-    
+    const $modal = _createModal(options)
     
     return {
         open() { },
